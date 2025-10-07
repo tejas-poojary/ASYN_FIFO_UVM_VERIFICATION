58eadce1c55d749288eb66004b6a6b10cb012365
tejas-poojary <tejaspoojary2003@gmail.com>
2025-10-07 07:27:31 +0530

    Create async_fifo_wr_sequence_item.sv

---
+++ src/async_fifo_wr_sequence_item.sv
@@ -0,0 +1,20 @@
+ import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "async_fifo_defines.sv"
+
+class wr_seq_item extends uvm_sequence_item;
+  rand logic winc;
+  rand logic [`DSIZE-1:0]wdata;
+  logic wfull;
+  
+  function new(string name="wr_seq_item");
+    super.new(name);
+  endfunction
+  
+  `uvm_object_utils_begin(wr_seq_item)
+    `uvm_field_int(wdata,UVM_ALL_ON+UVM_DEC)
+    `uvm_field_int(winc,UVM_ALL_ON+UVM_DEC)
+    `uvm_field_int(wfull,UVM_ALL_ON+UVM_DEC)
+  `uvm_object_utils_end
+  
+endclass
