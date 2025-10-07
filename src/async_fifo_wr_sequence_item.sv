46a53470a511aa3361eb040aa08afef352e4607b
tejas-poojary <tejaspoojary2003@gmail.com>
2025-10-07 23:22:51 +0530

    Update async_fifo_wr_sequence_item.sv

--- src/async_fifo_wr_sequence_item.sv
+++ src/async_fifo_wr_sequence_item.sv
@@ -6,6 +6,7 @@ class wr_seq_item extends uvm_sequence_item;
   rand logic winc;
   rand logic [`DSIZE-1:0]wdata;
   logic wfull;
+  logic wrst_n;
   
   function new(string name="wr_seq_item");
     super.new(name);
@@ -15,6 +16,9 @@ class wr_seq_item extends uvm_sequence_item;
     `uvm_field_int(wdata,UVM_ALL_ON+UVM_DEC)
     `uvm_field_int(winc,UVM_ALL_ON+UVM_DEC)
     `uvm_field_int(wfull,UVM_ALL_ON+UVM_DEC)
+    `uvm_field_int(wrst_n,UVM_ALL_ON+UVM_DEC)
   `uvm_object_utils_end
   
 endclass
+  
+  
