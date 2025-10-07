8ddb3fcffe856564c21f80e0dcc9026d0351a91c
tejas-poojary <tejaspoojary2003@gmail.com>
2025-10-07 23:22:20 +0530

    Update async_fifo_rd_sequence_item.sv

--- src/async_fifo_rd_sequence_item.sv
+++ src/async_fifo_rd_sequence_item.sv
@@ -6,6 +6,7 @@ class rd_seq_item extends uvm_sequence_item;
   rand logic rinc;
   logic [`DSIZE-1:0]rdata;
   logic rempty;
+  logic rrst_n;
   
   function new(string name="rd_seq_item");
     super.new(name);
@@ -15,6 +16,7 @@ class rd_seq_item extends uvm_sequence_item;
     `uvm_field_int(rdata,UVM_ALL_ON+UVM_DEC)
     `uvm_field_int(rinc,UVM_ALL_ON+UVM_DEC)
     `uvm_field_int(rempty,UVM_ALL_ON+UVM_DEC)
+    `uvm_field_int(rrst_n,UVM_ALL_ON+UVM_DEC)
   `uvm_object_utils_end
   
 endclass
